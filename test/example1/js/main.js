332a919d472b9439109e0325f8d5e394ca8f9000
RPerkins96 <rodperkins@comcast.net>
2014-03-09 09:59:51 -0700

    First Example
    Renamed libraries as well

---
+++ test/example1/js/main.js
@@ -0,0 +1,52 @@
+// Sets the require.js configuration for your application.
+require.config( {
+
+	// 3rd party script alias names
+	paths: {
+
+		// Core Libraries
+		"jquery": "../../libs/js/jquery",
+		"jquerymobile": "../../libs/js/jquerymobile/jquery.mobile-1.4.2",
+            	"underscore": "../../libs/js/lodash",
+            	"backbone": "../../libs/js/backbone",
+
+	},
+
+	// Sets the configuration for your third party scripts that are not AMD compatible
+	shim: {
+
+		"backbone": {
+			"deps": [ "underscore", "jquery" ],
+			"exports": "Backbone"
+		}
+
+	}
+
+});
+
+// Includes File Dependencies
+require([
+	"jquery",
+	"backbone",
+	"js/routers/mobileRouter"
+], function ( $, Backbone, Mobile ) {
+
+	$( document ).on( "mobileinit",
+
+		// Set up the "mobileinit" handler before requiring jQuery Mobile's module
+		function () {
+
+			// Prevents all anchor click handling including the addition of active button state and alternate link bluring.
+			$.mobile.linkBindingEnabled = false;
+
+			// Disabling this will prevent jQuery Mobile from handling hash changes
+			$.mobile.hashListeningEnabled = false;
+		}
+	)
+
+	require( [ "jquerymobile" ], function () {
+
+		// Instantiates a new Backbone.js Mobile Router
+		this.router = new Mobile();
+	});
+});
